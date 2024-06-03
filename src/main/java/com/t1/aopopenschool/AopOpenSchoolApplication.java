1aa25b2a8245300f3566dbe613bb55376e9e3670
Evgenii.Pichugov <eugepic@yandex.ru>
2024-06-03 17:21:54 +0300

    initial commit

---
+++ src/main/java/com/t1/aopopenschool/AopOpenSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.t1.aopopenschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopOpenSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopOpenSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/t1/aopopenschool/AopOpenSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1.aopopenschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopOpenSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
