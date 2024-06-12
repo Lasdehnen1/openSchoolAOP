0feb2cfd07524334c4dbe7efc1d503af1ac6f8e1
Evgenii.Pichugov <eugepic@yandex.ru>
2024-06-12 11:37:04 +0300

    EnableAsync annotation added

--- src/main/java/com/t1/aopopenschool/config/AsyncConfig.java
+++ src/main/java/com/t1/aopopenschool/config/AsyncConfig.java
@@ -2,11 +2,13 @@ package com.t1.aopopenschool.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig {
     @Bean(name = "asyncExecutor")
     public Executor asyncExecutor() {
